e957aaf46051052cb823d881272bb5c683cc91f2
Jazmingv <jaz.vainman@gmail.com>
2022-02-08 21:37:49 -0300

    tarea 5

---
+++ tarea-5/src/App.js
@@ -0,0 +1,14 @@
+//import React from 'react';
+import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <ItemListContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ tarea-5/src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+function ItemListContainer() {
+    return (
+      <>
+        <h3>Este es el ItemListContainer!</h3>
+      </>
+    );
+  }
+  
+  export default ItemListContainer;
